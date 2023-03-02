07c5abde2810b546f8f12c6ba9fe4ee90e259415
cinthia <cinthiasanchezr1997@gmail.com>
2023-03-01 22:18:52 -0800

    update routes

--- src/common/server.js
+++ src/common/server.js
@@ -5,28 +5,13 @@ import * as http from 'http';
 import * as os from 'os';
 import logger from '../api/middlewares/logger';
 import errorHandler from '../api/middlewares/error.handler';
-import corsMiddleware from '../api/middlewares/cors'
-
+import cors from 'cors';
 const app = new Express();
 
 export default class ExpressServer {
   constructor() {
     const root = path.normalize(`${__dirname}/../..`)
-    
-    app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,DELETE,OPTIONS');
-      res.header('Access-Control-Max-Age', 6000);
-      res.header('Cache-Control', 'must-revalidate');
-      res.header('Access-Control-Allow-Headers', 'X-Requested-With,content-type, Authorization, withCredentials, timeout, accept-language, ngsw-bypass');
-      res.header('Access-Control-Allow-Credentials', true);
-      if (req.method === 'OPTIONS') {
-        res.sendStatus(200);
-      } else {
-        next();
-      }
-    });
-    app.use(corsMiddleware);
+    app.use(cors());
     app.use(bodyParser.json({ limit: process.env.REQUEST_LIMIT || '100kb' }))
     app.use(
       bodyParser.urlencoded({
